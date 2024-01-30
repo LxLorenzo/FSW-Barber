aee4967538052a10f5f22e0193983e87e564fd44
Lorenzo Casa <lorenzocasa1235@gmail.com>
2024-01-29 21:37:51 -0300

    feat: add welcome message

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,19 @@
+import { format } from "date-fns/format";
 import Header from "../_components/header";
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
     <>
       <Header></Header>
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Lorenzo!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", {
+            locale: ptBR,
+          })}
+        </p>
+      </div>
     </>
   );
 }

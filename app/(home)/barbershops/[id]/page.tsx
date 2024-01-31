de517e234a479ee26df72ae13ab006a19fe70fd3
Lorenzo Casa <lorenzocasa1235@gmail.com>
2024-01-30 21:34:34 -0300

    feat: add ServiceItem component

---
+++ app/(home)/barbershops/[id]/page.tsx
@@ -0,0 +1,41 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+
+const BarbershopDetailsPage = async ({
+  params,
+}: BarbershopDetailsPageProps) => {
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    },
+    include: {
+      services: true,
+    },
+  });
+
+  if (!barbershop || !params.id) {
+    return null;
+  }
+  return (
+    <>
+      <BarbershopInfo barbershop={barbershop} />
+      <div className="px-5 py-6 flex flex-col gap-4">
+        {barbershop.services.map((service) => (
+          <ServiceItem service={service} key={service.id} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default BarbershopDetailsPage;
